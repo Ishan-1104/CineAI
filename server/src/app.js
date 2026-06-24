b6bc590cd0f03fc6e5dc3d45d2e7771c9e422ecf
Ishan <chhaparwalishan@gmail.com>
2026-06-25 02:09:46 +0530

    Serve React build and add React Router support
    Added support for serving React build and routing.

--- server/src/app.js
+++ server/src/app.js
@@ -1,30 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
 const movieRoutes = require("./routes/movie.routes");
-
 const recommendationRoutes = require("./routes/recommendation.routes");
-
 const authRoutes = require("./routes/auth.routes");
-
 const userRoutes = require("./routes/user.routes");
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/movies", movieRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-
-
-app.use("/api/recommendations",recommendationRoutes);
-
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "Movie Recommendation API Running",
-  });
-});
+app.use("/api/recommendations", recommendationRoutes);
 
 app.get("/api/test-db", async (req, res) => {
   const mongoose = require("mongoose");
@@ -35,6 +26,26 @@ app.get("/api/test-db", async (req, res) => {
     database:
       mongoose.connection.name,
   });
+});
+
+/* Serve React Build */
+app.use(
+  express.static(
+    path.join(
+      __dirname,
+      "../../client/dist"
+    )
+  )
+);
+
+/* React Router Support */
+app.get(/.*/, (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "../../client/dist/index.html"
+    )
+  );
 });
 
 module.exports = app;
